daa2301a75d52a5e6df84b7c22267c46ecf11dd0
Viktor2350 <ryjuk39@gmail.comm>
2019-11-15 23:05:05 +0200

    Updates

---
+++ precache-manifest.cf1798969412292ce0eed872a25653e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e57fe8a54e8f3b5c2da38258cceecdc",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "b681d26455e368782f5f",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.0f54525f.chunk.css"
+  },
+  {
+    "revision": "026614b8c989e6037a29",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.bd8578c9.chunk.js"
+  },
+  {
+    "revision": "b681d26455e368782f5f",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.aa0b1893.chunk.js"
+  },
+  {
+    "revision": "dffe8cd1e2fc699743fb",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.d2e8316a.js"
+  }
+]);
